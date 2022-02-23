fbbea867e431f5a569baf7738fd74e1fe8533456
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-02-23 08:47:11 +0100

    setClass

--- js/setClassNames.js
+++ js/setClassNames.js
@@ -5,9 +5,12 @@ const inpElementName = document.getElementById("elementName");
 
 function setElementStyle() {
   const elm = document.querySelector(inpElementName.value);
+  const elmList = elm.split(" ");
   const firstChar = String(pbSetStyle.value).substring(0, 1);
   const pureName = String(pbSetStyle.value).substring(1, pbSetStyle.value.length);
 
+
+  //Lav så man kan skrive remove for at fjerne element
   if (firstChar == '.') {
     elm.classList.add(pureName);
   } else if (firstChar == '#') {
